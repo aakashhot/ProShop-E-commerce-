c3eed9f6e2b68028fc21d8064f7e100a08a922ae
Aakash Hotchandani <aakashhotchandani22@gmail.com>
2021-04-18 01:01:09 +0530

    React setup

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <h1>Welcome to Proshop</h1>
+      </>
+    </div>
+  );
+}
+
+export default App;
