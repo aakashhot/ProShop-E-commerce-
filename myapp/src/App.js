16afb9d86a8f6ded3e6a6e5c4f5514dfef47109c
Aakash Hotchandani <aakashhotchandani22@gmail.com>
2021-08-22 12:18:02 +0530

    First commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom' 
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreens from './Screens/ProductScreens'
+import CartScreen from './Screens/CartScreen'
+import LoginScreen from './Screens/LoginScreen'
+import RegisterScreen from './Screens/RegisterScreen'
+import ProfileScreen from './Screens/ProfileScreen'
+import ShippingScreen from './Screens/ShippingScreen'
+import PaymentScreen from './Screens/PaymentScreen'
+import PlaceOrderScreen from './Screens/PlaceOrderScreen'
+import OrderScreen from './Screens/OrderScreen'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <>
-      <h1>Welcome to Proshop</h1>
-      </>
-    </div>
-  );
+    
+      <Router>
+      <Header/>
+      <main className='py-3'>
+      <Container>
+      <Route path='/order/:id' component={OrderScreen} />
+      <Route path='/shipping' component={ShippingScreen} />
+      <Route path='/payment' component={PaymentScreen} />
+      <Route path='/placeorder' component={PlaceOrderScreen} />
+      <Route path='/login' component={LoginScreen} />
+      <Route path='/register' component={RegisterScreen} />
+      <Route path='/profile' component={ProfileScreen} />
+      <Route path='/product/:id' component={ProductScreens} />
+      <Route path='/cart/:id?' component={CartScreen} />
+      <Route path='/' component={HomeScreen} exact />
+      
+      </Container>
+      </main>
+      <Footer/>
+      </Router>
+    
+  )
 }
 
 export default App;
---
+++ myapp/src/store.js
@@ -0,0 +1,46 @@
+import { createStore , combineReducers , applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools} from 'redux-devtools-extension'
+import {productListReducer ,productDetailsReducer} from './reducers/productReducers'
+import {cartReducer} from './reducers/cartReducer'
+import { userLoginReducer,userRegisterReducer,userDetailsReducer,userUpdateProfileReducer} from './reducers/userReducers'
+import {orderCreateReducer ,orderDetailsReducer,orderPayReducer} from './reducers/orderReducers'
+const reducer = combineReducers({
+    productList: productListReducer,
+    productDetails :productDetailsReducer,
+    cart: cartReducer,
+    userLogin:userLoginReducer,
+    userRegister:userRegisterReducer,
+    userDetails:userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay : orderPayReducer,
+})
+
+
+const cartItemsFromStorage = localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems')):[]
+
+const userInfoFromStorage = localStorage.getItem('userInfo')? JSON.parse(localStorage.getItem('userInfo')): null
+
+
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')? JSON.parse(localStorage.getItem('shippingAddress')): {}
+
+
+const intialState ={
+
+    cart:{cartItems:cartItemsFromStorage,shippingAddress:shippingAddressFromStorage},
+
+    userLogin :{ userInfo:userInfoFromStorage},
+}
+
+
+const middleWare = [thunk]
+
+
+const store = createStore(reducer,
+    intialState,
+    composeWithDevTools
+(applyMiddleware(...middleWare)))
+
+export default store
